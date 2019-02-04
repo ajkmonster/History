25673e617b94cce3bb04f1a95d021f632cd770ad
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-02-04 13:29:16 -0500

    Initial commit

---
+++ src/History.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class History {
+    public static void main(String[] args){
+        String[] arr = new String[10];
+        int x;
+        Scanner key = new Scanner(System.in);
+        System.out.println("Please write the last 10 presidents of the USA: ");
+        for (x=0; x<=9; x++){
+            arr[x] = key.next();
+        }
+        Scanner key1 = new Scanner(System.in);
+        String y = key1.next();
+        if (y.equalsIgnoreCase("history")) {
+            for (String s: arr){
+                System.out.println(s);
+            }
+        }
+    }
+}
